f8e13df13911c819882285c7af27575d34336c7b
Felipe Gabriel <68368843+felipegfsouza@users.noreply.github.com>
2020-10-22 10:10:18 -0300

    ex5 correção

--- ex05.php
+++ ex05.php
@@ -7,23 +7,44 @@
     <title>Document</title>
 </head>
 <body>
-    <form id="form1" name="form1" method="POST" action="triangulo.php">
-    <p>
-        <label for="ladoa">Lado A </label>
-        <input type="text" name="ladoa" id="ladoa">
-    </p>
-    <p>
-        <label for="ladoa">Lado B </label>
-        <input type="text" name="ladob" id="ladob">
-    </p>
-    <p>
-        <label for="ladoa">Lado C</label>
-        <input type="text" name="ladoc" id="ladoc">
-    </p>
-    <p>
-        <input type="submit" name="enviar" id="enviar" value="Enviar">
-    </p>
-    </form>
-   
+    <center>
+       <form method="post" action="#">
+			<label>V1: </label>
+			<input type="text"  name="V1" /><br />
+
+			<label>V2: </label>
+			<input type="text"  name="V2" /><br />
+
+			<label>V3: </label>
+			<input type="text"  name="V3" /><br />
+			<input type="submit" value="OK" />
+		</form>
+
+		<?php
+		if($_POST["V1"]!="" and $_POST["V2"]!="" and $_POST["V3"]!=""){
+			
+
+			
+
+			if ($_POST["V1"]==$_POST["V2"] and $_POST["V2"]==$_POST["V3"]) {
+				echo "<br>Equilatero";
+			}elseif ($_POST["V1"]!=$_POST["V3"] and $_POST["V1"]!=$_POST["V2"] and $_POST["V2"]!=$_POST["V3"]){
+					echo "<br>Escaleno";
+			}else{
+				echo "<br>Isosceles";
+			}
+
+		}else{
+			echo "digite um valor";
+		}
+		?>
+
+    </center>
+<
+
 </body>
 </html>
+
+<?php 
+
+?>

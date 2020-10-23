1a12c66c77a820a0e96a63037e10c40cd5941a39
Felipe Gabriel <68368843+felipegfsouza@users.noreply.github.com>
2020-10-23 17:07:06 -0300

    Update ex07

--- ex07.php
+++ ex07.php
@@ -8,6 +8,35 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h2 align="center">Atividade 7</h2>
+    <center>
+    <form method="GET"> 
+    Nome do Livro:<input type="text"name="nome"size="30"><br><br>
+    Categoria:<select name="cat">
+  <option value="Suspense">Suspense</option> 
+  <option value="Ação" >Ação</option>
+  <option value="Aventura" >Aventura</option>
+  <option value="Romance" >Romance</option> 
+  <option value="Terror" >Terror</option>
+  <option value="Sci-fi" >Sci-fi</option>
+</select><br><br>
+    Usuário:<input type="radio" Value ="Aluno" name="usu">Aluno
+            <input type="radio" Value ="Professor" name="usu">Professor<br><br>
+    <input type="submit" value="Confirmar"><br><br>
+</form>
+<?php
+$usu = $_GET['usu'];
+$nome = $_GET['nome'];
+$cat = $_GET['cat'];
+switch ($usu) {
+    case "Aluno":
+        echo "Nome: $nome<br>Categoria: $cat<br>Usuário: $usu <br> Devolução: 3 dias";
+    break;
+    case "Professor":
+        echo "Nome: $nome<br>Categoria: $cat<br>Usuário: $usu <br> Devolução: 10 dias";
+    break;
+}
+?>   
+</center>
 </body>
 </html>
